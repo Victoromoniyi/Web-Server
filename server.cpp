80fb83a732f578ff9d33af048c9fdf2e32ac4353
victoromoniyi <omoniyiv@cardiff.ac.uk>
2021-12-10 20:44:41 +0000

    added getter functions, test function for connection

--- server.cpp
+++ server.cpp
@@ -9,5 +9,35 @@ WSL::simpSocket::simpSocket(int domain, int typeService,
     address.sin_port = htons(port);
 
     //Starting the connection
-    connection = (domain, typeService, protocol);
+    sock = (domain, typeService, protocol);
+    testConnection(sock);
+    //Testing establish connection
+    connection = networkConnect(sock, address);
+    testConnection(connection);
+}
+
+
+void WSL::simpSocket::testConnection(int testConnectionType)
+{
+    // Confirms socket/connection has been properly established
+    if (testConnectionType < 0)
+        {
+            std::cerr << "Failed to connect\n";
+            exit(1);
+        }
+}
+
+struct sockaddr_in WSL::simpSocket::getAddress()
+{
+    return address;
+}
+
+int WSL::simpSocket::getSock()
+{
+    return sock;
+}
+
+int WSL::simpSocket::getConnection()
+{
+    return connection;
 }
--- server.hpp
+++ server.hpp
@@ -4,18 +4,28 @@
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <iostream>
 
 namespace WSL
 {
     class simpSocket
     {
         private:
+        int sock;
         int connection;
         struct sockaddr_in address;
 
         public:
+        //Constructor
         simpSocket(int domain, int typeService, int protocol,
          int port, u_long interface);
+         //Virtual function to connect to network
+         virtual int networkConnect(int sock, struct sockaddr_in address) = 0;
+         void testConnection(int);
+         //Getter functions
+         struct sockaddr_in getAddress();
+         int getSock();
+         int getConnection();
     };
 } 
 
